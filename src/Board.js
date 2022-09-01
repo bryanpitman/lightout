3a1b0e04d6a0844f73a88db836d1a6d705797f12
Margaret Sun <hi.margaret.sun@gmail.com>
2022-08-31 17:45:14 -0700

    make htmlBoard

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Cell from "./Cell";
 import "./Board.css";
-import { trueOrFalse } from "../utils";
+import { trueOrFalse } from "./utils";
 
 /** Game board of Lights out.
  *
@@ -28,19 +28,32 @@ import { trueOrFalse } from "../utils";
  *
  **/
 
-function Board({ nrows, ncols, chanceLightStartsOn }) {
+function Board({ nrows = 5, ncols = 5, chanceLightStartsOn=0.5 }) {
   const [board, setBoard] = useState(createBoard());
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   function createBoard() {
     let initialBoard = [];
-    initialBoard = new Array(nrows).fill(true).map(() => new Array(ncols).fill(trueOrFalse()));
+    initialBoard = Array.from({length: nrows}).map(
+      row => Array.from({length: ncols}).map(
+          cell => Math.random() < chanceLightStartsOn
+      )
+  );
+    console.log("initial board",initialBoard)
     return initialBoard;
   }
 
+  /** iterates over initalBoard and checks for false values
+   * returns true/false  */
   function hasWon() {
-    // TODO: check the board in state to determine whether the player has won.
-  }
+    // let won;
+
+    // for (let row of initialBoard){
+    //   won = row.includes(false)
+    //   }
+    }
+
+
 
   function flipCellsAround(coord) {
     setBoard(oldBoard => {
@@ -55,8 +68,17 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
       };
 
       // TODO: Make a (deep) copy of the oldBoard
+      const deepCopy = [...board]
+      // const deepCopy = board.map(b => [...b])
 
       // TODO: in the copy, flip this cell and the cells around it
+      setBoard(deepCopy => {
+
+        //get the cell
+        //find the surrounding cells
+        //switch true/false
+
+      })
 
       // TODO: return the copy
     });
@@ -67,6 +89,24 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
   // TODO
 
   // make table board
+  function generateHtmlTable(){
+    let htmlBoard = []
+    for (let y = 0; y < nrows; y++){
+      let row = []
+      for(let x = 0; x < ncols; x++){
+        row.push(<Cell flipCellsAroundMe={0} isLit={board[y][x]}/>)
+      }
+      htmlBoard.push(<tr>{row}</tr>)
+    }
+    return htmlBoard
+  }
+  return (
+  <table border="1px">
+    <tbody>
+      {generateHtmlTable()}
+    </tbody>
+  </table>
+  )
 
   // TODO
 }
